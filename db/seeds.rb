47a7462321947e032dffbbada49026412f114dc6
Kristina Vragovic <k.vragovic@gmail.com>
2017-07-16 21:43:13 -0500

    Seed file populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker' 
+
+8.times do 
+  post = Post.create(
+    title: Faker::StarWars.quote,
+    body: Faker::Lorem.paragraph(8)
+    )
+end
+
+
+15.times do 
+  comment = Comment.create(
+    name: Faker::StarWars.character,
+    body: Faker::StarWars.wookiee_sentence,
+    post_id: Post.all.sample.id 
+    )
+end
+
+
